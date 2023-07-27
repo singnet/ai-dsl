c129bb918dd1dc92ea08e4b0e21315581ccbadbc
Nil Geisweiller <ngeiswei@gmail.com>
2023-07-27 13:16:35 +0300

    Annotate grpc outputs
    This is necessary in order for mypy to catch type checking error
    associated to service composition.

--- experimental/proto-checking/python/services_client_invalid_composition.py
+++ experimental/proto-checking/python/services_client_invalid_composition.py
@@ -1,5 +1,5 @@
 import grpc
-import services_pb2
+from services_pb2 import FooIn, FooOut, BarIn, BarOut, BazIn, BazOut
 import services_pb2_grpc
 
 def run_client():
@@ -9,23 +9,23 @@ def run_client():
     # Test baz . foo . bar (invalid)
     print("=== Test baz . foo . bar (invalid) ===")
 
-    bar_request = services_pb2.BarIn(bar_int=42)
+    bar_request = BarIn(bar_int=42)
     print("type(bar_request) = {}".format(type(bar_request)))
     print("bar_request = {}".format(bar_request))
 
-    bar_response = stub.bar_rpc(bar_request)
+    bar_response : BarOut = stub.bar_rpc(bar_request)
     print("type(bar_response) = {}".format(type(bar_response)))
     print("bar_response = {}".format(bar_response))
 
     # Pass the output of bar indirectly, via re-initialization into
     # the input of foo.  It fails.
-    foo_request = services_pb2.FooIn(foo_bool=bar_response.bar_str)
-    foo_response = stub.foo_rpc(foo_request)
+    foo_request = FooIn(foo_bool=bar_response.bar_str)
+    foo_response : FooOut = stub.foo_rpc(foo_request)
     print("type(foo_response) = {}".format(type(foo_response)))
     print("foo_response = {}".format(foo_response))
 
-    baz_request = services_pb2.BazIn(baz_str=foo_response.foo_int)
-    baz_response = stub.baz_rpc(baz_request)
+    baz_request = BazIn(baz_str=foo_response.foo_int)
+    baz_response : BazOut = stub.baz_rpc(baz_request)
     print("type(baz_response) = {}".format(type(baz_response)))
     print("baz_response = {}".format(baz_response))
 
