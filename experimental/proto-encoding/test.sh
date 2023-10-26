0a937721f0bd454bfbe8e1a529a1b8503ba8f045
Nil Geisweiller <ngeiswei@gmail.com>
2023-10-26 14:36:42 +0300

    Add hexdump and protoscope formats

--- experimental/proto-encoding/test.sh
+++ experimental/proto-encoding/test.sh
@@ -2,10 +2,18 @@
 echo "* Encode the following protobuf message:"
 cat test.msg
 cat test.msg | protoc --encode=test.Greet test.proto > test.msg.encoded
+
+# Print encoded message
 echo
-echo "Encoded message:"
+echo "* Encoded message in Wire format:"
 cat test.msg.encoded
 echo
+echo
+echo "* Encoded message in Hexadecimal format:"
+hexdump test.msg.encoded
+echo
+echo "* Encoded message in Protoscope format:"
+protoscope -explicit-length-prefixes -explicit-wire-types test.msg.encoded
 
 # Decode message
 echo
